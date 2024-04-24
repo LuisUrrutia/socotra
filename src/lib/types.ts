896e09bed5682b0791f1803cde41386a9b5f7d1b
Luis Urrutia <luis@urrutia.me>
2024-04-24 18:46:45 -0400

    feat: add base types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,16 @@
-export type CardVariant = 'primary' | 'secondary';
+export type CardVariant = "primary" | "secondary";
+
+export type Vehicle = {
+  id: string;
+  make: string;
+  model: string;
+  year: number;
+  value: number;
+};
+
+export type Driver = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  licenseNumber: string;
+};
